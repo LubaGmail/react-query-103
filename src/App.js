2f3b3900e5d8ab59f4d6801069c166cd2a244058
LL Powe <powe2009@gmail.com>
2022-01-28 13:37:55 -0500

    useQuery select

--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  useQuery,
+  gql
+} from "@apollo/client";
+import { selectionSetMatchesResult } from '@apollo/client/cache/inmemory/helpers';
 
-function App() {
+const GET_DOGS = gql`
+  query dogs {
+    dogs {
+      id
+      breed
+    }
+  }
+`
+
+const GET_PHOTO = gql`
+  query dog($breed: String!) {
+    dog(breed: $breed) {
+      id
+      displayImage
+    }
+  }
+`
+
+const App = () => {
+  const client = new ApolloClient({
+    uri: "https://71z1g.sse.codesandbox.io/",
+    cache: new InMemoryCache()
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h4>Wrap components inside ApolloProvider</h4>
+        <Main />
+      </div>
+    </ApolloProvider>
   );
+}
+
+const Main = () => {
+  // setState is asynchronous, so this.state will still contain the previous values 
+  //  if you try to use it right after calling setState.
+  const [breed, setBreed] = React.useState(null)
+  const {loading, error, data, previousData} = useQuery(
+    GET_DOGS,
+    {
+      fetchPolicy: "network-only",              // first fetch
+      nextFetchPolicy: "cache-and-network"
+    }
+  )
+
+  if (loading) return 'Loading...'
+  if (error) return `${error.message}`
+ 
+  const handleChange = (ev) => {
+    setBreed(ev.target.value)
+    // is not updated right away
+    // console.log('handleChange', breed)
+  }
+ 
+  return(
+    <>
+      <h4>Maintain state</h4>
+
+      <div className='selectDiv'>
+        <DogShow data={data} handleChange={handleChange}
+        />
+      </div>
+      <div className='photoDiv'>
+        {
+          breed && <DogPhoto breed={breed} />
+        }
+      </div>
+ 
+    </>
+  )
+}
+
+const DogShow = ({data, previousData, handleChange}) => {
+  // console.log('data/previousData', data, previousData)
+
+  // const onHandleChange = (ev) => {
+  //   console.log('onHangleChange', ev.target.value)
+  // }
+
+  return (
+    <>
+      <h4>Dog Show</h4>
+      <div className="custom-select">
+
+        <select name='dogs'
+          onChange={handleChange}
+        >
+          {
+            data.dogs.map((el) => (
+              <option key={el.id}
+                value={el.breed}
+              >
+                {el.breed}
+              </option>
+            ))
+          }
+
+        </select>
+      </div>
+
+    </>
+  )
+}
+
+const DogPhoto = ({breed}) => {
+  // correct value here
+  // console.log('DogPhoto', breed)
+ 
+  const {loading, error, data, previousData, networStatus} = useQuery(
+    GET_PHOTO,
+    {
+      variables: { breed },
+      notifyOnNetworkStatusChange: true,
+    }
+  )
+
+  if (loading) return 'Loading...';
+  if (error) return `${error.message}`
+  // console.log('data', data)
+
+  return (
+    <>
+      <div>
+        <img src={data.dog.displayImage}  />
+      </div>
+    </>
+  )
+
 }
 
 export default App;
